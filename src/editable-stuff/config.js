b515a6c81b93bf0067d704aea478d79ba2b3a461
mehni <lozomehni28@gmail.com>
2022-04-30 18:40:09 +0100

    my port

---
+++ src/editable-stuff/config.js
@@ -0,0 +1,93 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "Aziz",
+  middleName: "",
+  lastName: "Chebaane",
+  message: "Obsessed about making Software",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/MehniLozo",
+    },
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/aziz-chebaane-995546234/",
+    },
+    {
+      image: "fa-twitter",
+      url: "https://twitter.com/Chebaane7",
+    },
+  ],
+};
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/me2.jpg"),
+  imageSize: 375,
+  message:
+    "This is Aziz Chebaane. A Computer Science student at ISIMM."+ '\r\n' +" Detailed-oriented with a get-it-done,on-time and high quality DevOps mindset. I'm mostly passionate about Cloud and AI technologies. Twitter is my free time space ",
+  resume: require("../editable-stuff/Chebaane.pdf"),
+};
+const repos = {
+  show: true,
+  heading: "Recent Projects",
+  gitHubUsername: "MehniLozo",
+  reposLength: 4,
+  specificRepos: [],
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "Skills",
+  hardSkills: [
+    { name: "Python", value: 90 },
+    { name: "Data Structures", value: 85 },
+    { name: "C", value: 75 },
+    { name: "Golang", value: 60 },
+    {name: "Java", value: 70},
+    { name: "JavaScript", value: 90 },
+    { name: "HTML/CSS", value: 70 },
+    { name: "React", value: 65 },
+    { name: "NodeJS", value: 80 },
+
+  ],
+    tools: [
+    { name: "NumPy", value: 50 },
+    { name: "Pandas", value: 60},
+    { name: "Scikit-learn", value: 40},
+    { name: "SQL/NoSQL", value: 75 },
+    { name: "Kubernetes", value: 50 },
+    { name: "Docker", value: 70 ,icon: require("../editable-stuff/Docker.png")},
+    { name: "Linux", value: 50 },
+    { name: "Azure DevOps", value: 50 },
+
+  ],
+  softSkills: [
+    { name: "Goal-Oriented", value: 80 },
+    { name: "Collaboration", value: 90 },
+    { name: "Positivity", value: 75 },
+    { name: "Adaptability", value: 85 },
+    { name: "Problem Solving", value: 75 },
+    { name: "Empathy", value: 90 },
+    { name: "Organization", value: 70 },
+    { name: "Creativity", value: 90 },
+  ]
+
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Get In Touch",
+  message:
+    "I'm currently looking for Software related opportunities, if i seem to be competent filling your void please dont hesitate to get in touch via",
+  email: "chebaaneaziz28@gmail.com",
+};
+export { navBar, mainBody, about, repos, skills,getInTouch };
