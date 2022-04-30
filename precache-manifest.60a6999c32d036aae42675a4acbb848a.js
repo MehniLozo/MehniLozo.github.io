ac69da53629c77a3ee14005e189c313bb15cdca0
mehni <lozomehni28@gmail.com>
2022-04-30 21:51:43 +0100

    Updates

---
+++ precache-manifest.60a6999c32d036aae42675a4acbb848a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1677d27669cd976fe2a4851b082ca9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5806dbbfb13067cbdd",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1814ac6063dc9cb107d6",
+    "url": "/static/js/2.f3591468.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.f3591468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5806dbbfb13067cbdd",
+    "url": "/static/js/main.2a95cd23.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "c9283e867c1336fd15c03267c10f434f",
+    "url": "/static/media/Chebaane.c9283e86.pdf"
+  },
+  {
+    "revision": "bbdec1b7eae0a3610ccf2b8bc23a9d12",
+    "url": "/static/media/me2.bbdec1b7.jpg"
+  }
+]);
